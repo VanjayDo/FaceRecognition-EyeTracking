1fca51184fbabb798a65fdf631dcd971f8b2c6d8
VanjayDo <vanjaydo@gmail.com>
2018-07-08 20:26:55 +0800

    fix(文件调整): 去除mysql2redis容器的单独构造;

--- set-mysql-trigger.sql
+++ set-mysql-trigger.sql
@@ -1,7 +1,8 @@
 DROP trigger IF EXISTS face_eye.face_characteristic_to_redis;
 
 DELIMITER $$
-CREATE TRIGGER face_eye.face_characteristic_to_redis AFTER INSERT ON face_eye.face_facecharacteristic
+CREATE TRIGGER create_trigger AFTER
+CREATE TRIGGER face_characteristic_to_redis AFTER INSERT ON face_eye.face_recognizing_facecharacteristic
 FOR EACH ROW
 BEGIN
 SET @ret1=redis_servers_set_v2("redis", 6379);
